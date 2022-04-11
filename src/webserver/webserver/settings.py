861a6bf55044b9dd4c9654dc45919b2ad1308625
Retrontology <retrontology@hotmail.com>
2022-04-10 19:32:15 -0500

    move messages template

--- src/webserver/webserver/settings.py
+++ src/webserver/webserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os.path import join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'webserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [join(BASE_DIR, 'webserver', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
