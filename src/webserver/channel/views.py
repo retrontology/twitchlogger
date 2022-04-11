837a977577d4199f4108d824d3c39a54778d8cb4
Retrontology <retrontology@hotmail.com>
2022-04-10 19:48:23 -0500

    add limit and page

--- src/webserver/channel/views.py
+++ src/webserver/channel/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
 from datetime import datetime
-from webserver.messages import get_channel_messages, get_db
+from webserver.messages import get_channel_messages, get_db, DEFAULT_LIMIT
 
 def index(request):
     raise Http404("Incorrect usage") 
 
 def channel(request, channel):
+    print()
     template = loader.get_template('channel/index.html')
     dbs = get_db().list_collection_names()
     if channel.lower() in dbs:
-        messages = get_channel_messages(channel)
+        messages = get_channel_messages(
+            channel=channel,
+            limit=int(request.GET.get('limit', DEFAULT_LIMIT)),
+            page=int(request.GET.get('page', 0))
+        )
         return HttpResponse(template.render({'messages': messages, 'channel': channel}, request))
     else:
         raise Http404("Channel not found in database") 
