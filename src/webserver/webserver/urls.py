599e989a698daba940d98c66bd05229c4acde502
Retrontology <retrontology@hotmail.com>
2022-04-30 01:02:58 -0500

    test root redirect

--- src/webserver/webserver/urls.py
+++ src/webserver/webserver/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('channel/', include('channel.urls')),
-    path('user/', include('user.urls'))
+    path('user/', include('user.urls')),
+    path('', include(views.index))
 ]
