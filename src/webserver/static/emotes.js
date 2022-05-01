c852153b3b03f3bf88ff69b0c267e4920a9f141a
Retrontology <retrontology@hotmail.com>
2022-05-01 02:51:50 -0500

    change messages to id

--- src/webserver/static/emotes.js
+++ src/webserver/static/emotes.js
@@ -1,20 +1,88 @@
-async function fetch_ffz_channel_emotes(channel) {
-    channel = channel.toLowerCase()
-    let url = 'https://api.frankerfacez.com/v1/room/' + channel
-    let response = await fetch('/readme.txt');
+async function fetch_ffz_emotes(url) {
+    let response = await fetch(url);
     if (response.status === 200) {
         const data = await response.json();
-        var emotes = []
+        var emotes = [];
         for (var emote_set in data.sets) {
-            for (var emote of data.sets[emote_set].emoticons) {
-
-            }
+            emotes = emotes.concat(data.sets[emote_set].emoticons);
         }
         return emotes;
+    }
+    else {
+        return [];
+    }
+}
+
+async function fetch_ffz_channel_emotes(channel_id) {
+    let url = 'https://api.frankerfacez.com/v1/room/id/' + channel_id
+    const emotes = await fetch_ffz_emotes(url);
+    return emotes;
+}
+
+async function fetch_ffz_global_emotes() {
+    let url = 'https://api.frankerfacez.com/v1/set/global'
+    const emotes = await fetch_ffz_emotes(url);
+    return emotes;
+}
+
+async function fetch_bttv_channel_emotes(channel_id) {
+    let url = 'https://api.betterttv.net/3/cached/users/twitch/' + channel_id;
+    if (response.status === 200) {
+        const data = await response.json();
+        const emotes = data.channelEmotes.concat(data.sharedEmotes);
+        return emotes;
+    }
+    else {
+        return [];
     }
 }
 
 async function fetch_bttv_global_emotes() {
-    url = 'https://api.betterttv.net/3/cached/emotes/global'
+    let url = 'https://api.betterttv.net/3/cached/emotes/global';
+    if (response.status === 200) {
+        const emotes = await response.json();
+        return emotes;
+    }
+    else {
+        return [];
+    }
 }
 
+async function fetch_7tv_global_emotes() {
+    let url = 'https://api.7tv.app/v2/emotes/global'
+    if (response.status === 200) {
+        const emotes = await response.json();
+        return emotes;
+    }
+    else {
+        return [];
+    }
+}
+
+async function fetch_7tv_channel_emotes(channel) {
+    let url = 'https://api.7tv.app/v2/users/' + channel + '/emotes'
+    if (response.status === 200) {
+        const emotes = await response.json();
+        return emotes;
+    }
+    else {
+        return [];
+    }
+}
+
+async function parse_emotes() {
+
+    ffz_global = await fetch_ffz_global_emotes();
+    bttv_global = await fetch_bttv_global_emotes();
+    seventv_global = await fetch_7tv_global_emotes();
+
+    ffz_channels = {};
+    bttv_channels = {};
+    seventv_channels = {};
+
+    for (let i in this.rows) {
+        let row = table.rows[i];
+        let channel_id = row.getAttribute('data-channel-id');
+        let channel = 
+    }
+}
