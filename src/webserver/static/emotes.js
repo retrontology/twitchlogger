adf5c331a6d0051e592efb1f12fe4b5f2f885685
Retrontology <retrontology@hotmail.com>
2022-05-07 04:05:51 -0500

    revert regex

--- src/webserver/static/emotes.js
+++ src/webserver/static/emotes.js
@@ -106,11 +106,12 @@ function parse_bttv_emotes(cell, global_emotes, channel_emotes) {
         if (emote == undefined) {
             continue;
         }
-        let regexp = new RegExp('\b' + emote['code'] + '\b');
-        console.log(regexp)
+        let regexp = new RegExp('\\b' + emote['code'] + '\\b');
+
         let index = 0;
         while (index < cell.childNodes.length) {
             node = cell.childNodes[index];
+            console.log(node)
             if ((node.classList != undefined) && (node.classList.contains("content-fragment"))) {
                 var match = regexp.exec(node.innerHTML);
                 if (match) {
