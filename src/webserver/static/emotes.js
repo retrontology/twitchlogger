60a89869ab9b5a2b5f6769e1064ee40b7c9ef353
Retrontology <retrontology@hotmail.com>
2022-05-07 21:07:08 -0500

    debug child nodes again

--- src/webserver/static/emotes.js
+++ src/webserver/static/emotes.js
@@ -213,6 +213,7 @@ function replace_twitch_emotes(cell, emote_indexes) {
         emote_indexes.sort(compare_indexes);
         let last_end = 0;
         let index = 1;
+        console.log(cell.childNodes);
         for (var emote_index of emote_indexes) {
             
             let url = emote_index[0];
