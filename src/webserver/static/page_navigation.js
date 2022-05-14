3f697eaf83e8dcda012b8d74cd6035b24f4489c2
Retrontology <retrontology@hotmail.com>
2022-05-14 03:48:17 -0500

    partial commit

--- src/webserver/static/page_navigation.js
+++ src/webserver/static/page_navigation.js
@@ -1,3 +1,17 @@
 function change_page() {
     window.location.href = this.getAttribute('data-url') + '&page=' + this.value;
 }
+
+function populate_page_select(element) {
+    element.childNodes
+}
+
+function load_navigation() {
+    let navigation_elements = document.getElementsByClassName('page-navigation');
+    for (var nav_span of navigation_elements) {
+        if (nav_span.tagName == "SPAN") {
+            populate_page_select(nav_span)
+            nav_span.childNodes
+        }
+    }
+}
