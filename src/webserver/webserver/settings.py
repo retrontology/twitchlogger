6c2e9577bb6f9e2e33acf5e808c503d86d90968f
Retrontology <retrontology@hotmail.com>
2022-05-14 02:46:34 -0500

    remove app_dirs parameter

--- src/webserver/webserver/settings.py
+++ src/webserver/webserver/settings.py
@@ -58,7 +58,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [join(BASE_DIR, 'webserver', 'templates')],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
