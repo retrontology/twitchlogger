6150739c3ba1432ef8695ee34fc96f3d928cff3f
Retrontology <retrontology@hotmail.com>
2022-05-15 00:56:39 -0500

    append to proper element

--- src/webserver/static/page_navigation.js
+++ src/webserver/static/page_navigation.js
@@ -8,8 +8,8 @@ function populate_page_select(element) {
     let last_page = element.getElementsByClassName('last-page')[0].textContent;
     last_page = parseInt(last_page.slice(0, -3));
 
-    let current_page = element.getElementsByClassName('page-selection')[0];
-    current_page = current_page.getElementsByTagName('option')[0].value;
+    let page_selection = element.getElementsByClassName('page-selection')[0];
+    let current_page = page_selection.getElementsByTagName('option')[0].value;
     current_page = parseInt(current_page);
 
     if (current_page > MIN_PAGE) {
@@ -17,16 +17,16 @@ function populate_page_select(element) {
             let option = document.createElement('option');
             option.value = i;
             option.text = i;
-            element.insertBefore(option, element.firstChild);
+            page_selection.insertBefore(option, element.firstChild);
         }
     }
-    
+
     if (current_page < last_page) {
         for (var i = current_page + 1; i <= last_page; i++) {
             let option = document.createElement('option');
             option.value = i;
             option.text = i;
-            element.appendChild(option);
+            page_selection.appendChild(option);
         }
     }
 }
