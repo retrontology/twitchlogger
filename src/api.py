3113fe5589e9f66b8c05dba7bd7080bdfc1f3726
Retrontology <retrontology@hotmail.com>
2022-09-29 23:40:37 -0500

    isolate chatbot

---
+++ src/api.py
@@ -0,0 +1,85 @@
+from http.server import BaseHTTPRequestHandler
+from noSQLogger import noSQLogger
+from logging import getLogger
+from urllib.parse import unquote
+import json
+
+class TwitchLoggerAPI(BaseHTTPRequestHandler):
+
+    logger = getLogger(__name__)
+
+    def __init__(self, bot: noSQLogger, *args, **kwargs):
+        self.bot = bot
+        super().__init__(*args, **kwargs)
+
+    def do_DELETE(self):
+        path, args = self.parse_path()
+        if path != '/channel':
+            self.not_found()
+        else:
+            if not 'channel' in args:
+                self.malformed()
+            else:
+                if self.bot.remove_channel(args['channel']):
+                    result = {
+                        'result': True
+                    }
+                    self.respond(200, json.dumps(result))
+                else:
+                    result = {
+                        'result': False
+                    }
+                    self.respond(500, json.dumps(result))
+            
+
+    def do_GET(self):
+        path, args = self.parse_path()
+        if path != '/channel':
+            self.not_found()
+        else:
+            channels = json.dumps({'channels': self.bot.get_channels()})
+            self.respond(200, channels)
+            
+
+    def do_PUT(self):
+        path, args = self.parse_path()
+        if path != '/channel':
+            self.not_found()
+        else:
+            if not 'channel' in args:
+                self.malformed()
+            else:
+                if self.bot.add_channel(args['channel']):
+                    result = {
+                        'result': True
+                    }
+                    self.respond(200, json.dumps(result))
+                else:
+                    result = {
+                        'result': False
+                    }
+                    self.respond(500, json.dumps(result))
+
+    def parse_path(self):
+        path = unquote(self.path)
+        args = path.split('?', 1)
+        out_args = {}
+        if len(args) > 1:
+            path = args[0]
+            args = args[1].split('&')
+            for arg in args:
+                key, value = arg.split('=')
+                out_args[key] = value
+        return path, out_args
+
+    def respond(self, code, message=None):
+        self.send_response(code)
+        self.send_header('Content-Length', len(message))
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+    def not_found(self):
+        self.respond(404, 'Not found')
+    
+    def malformed(self):
+        self.respond(400, 'Bad request')
---
+++ src/run.py
@@ -0,0 +1,61 @@
+from noSQLogger import noSQLogger
+from functools import partial
+from api import TwitchLoggerAPI
+import retroBot.config
+import os
+import logging
+import logging.handlers
+from http.server import ThreadingHTTPServer
+from threading import Thread
+
+def main():
+    logger = setup_logger('retroBot')
+    config = retroBot.config.config('config.yaml')
+    bot, bot_thread = setup_bot(config)
+    setup_api(bot, config['api'])
+
+def setup_api(bot, config):
+    api_handler = partial(TwitchLoggerAPI, bot)
+    api_server = ThreadingHTTPServer((config['host'], config['port']), api_handler)
+    api_server.serve_forever()
+
+def setup_bot(config):
+    bot = noSQLogger(
+        config['twitch']['username'], 
+        config['twitch']['client_id'], 
+        config['twitch']['client_secret'],
+        dbhosts=config['mongo']['hosts'],
+        dbusername=config['mongo']['username'],
+        dbpassword=config['mongo']['password'],
+        dboptions=config['mongo']['options'],
+        defaultauthdb=config['mongo']['authdb'],
+        dbname=config['mongo']['dbname'],
+        ffz=True,
+        bttv=True,
+        seventv=True
+    )
+    bot_thread = Thread(target=bot.start, daemon=True).start()
+    return bot, bot_thread
+
+def setup_logger(logprefix, logname=None, logpath=""):
+    if not logpath or logpath == "":
+        logpath = os.path.join(os.path.dirname(__file__), 'logs')
+    else:
+        logpath = os.path.abspath(logpath)
+    if not os.path.exists(logpath):
+        os.mkdir(logpath)
+    logger = logging.getLogger(logname)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.handlers.TimedRotatingFileHandler(os.path.join(logpath, logprefix), when='midnight')
+    stream_handler = logging.StreamHandler()
+    form = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+    file_handler.setFormatter(form)
+    stream_handler.setFormatter(form)
+    file_handler.setLevel(logging.DEBUG)
+    stream_handler.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    return logger
+
+if __name__ == "__main__":
+    main()
